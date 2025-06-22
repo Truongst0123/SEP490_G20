a14ee2b8c0e78b94d411555d1e8a69c3d0d7d1a3
Truongst0123 <truongnqhe140182@fpt.edu.vn>
2025-06-22 22:55:51 +0700

    update staff

--- management/src/main/java/com/system/restaurant/management/repository/TableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.system.restaurant.management.repository;
-
-import com.system.restaurant.management.entity.RestaurantTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TableRepository extends JpaRepository<RestaurantTable, Integer> {
-}
