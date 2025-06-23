415e27ef01869f43e743877612abaaea4cf327a7
Tran Nam Thanh <thanhtnhe172186@fpt.edu.vn>
2025-06-23 21:37:08 +0700

    <update>[thanhtn]: update manage table

--- management/src/main/java/com/system/restaurant/management/controller/TableController.java
+++ management/src/main/java/com/system/restaurant/management/controller/TableController.java
@@ -14,30 +14,40 @@ import java.util.List;
 public class TableController {
     private final TableService service;
 
-    @GetMapping
+    @GetMapping("/getAll")
     public ResponseEntity<List<RestaurantTable>> getAll() {
         return ResponseEntity.ok(service.findAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("getById/{id}")
     public ResponseEntity<RestaurantTable> getById(@PathVariable Integer id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<RestaurantTable> create(@RequestBody RestaurantTable table) {
         return ResponseEntity.ok(service.create(table));
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<RestaurantTable> update(@PathVariable Integer id,
                                                   @RequestBody RestaurantTable table) {
         return ResponseEntity.ok(service.update(id, table));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("delete/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("getAllTypes")
+    public ResponseEntity<List<String>> getAllTypes() {
+        return ResponseEntity.ok(service.getAllTableTypes());
+    }
+
+    @GetMapping("/getByTableType/{type}")
+    public ResponseEntity<List<RestaurantTable>> getByType(@PathVariable String type) {
+        return ResponseEntity.ok(service.getByTableType(type));
     }
 }
