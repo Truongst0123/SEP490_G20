7e1ea9f5142d1fdde1d97f56e42e635ff34c3385
Dau Ba Anh Dung <anhdungkudo@gmail.com>
2025-06-25 23:24:08 +0700

    Create booking page

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,13 +1,13 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
+import Booking from './pages/Booking';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-       
+        <Route path="/booking" element={<Booking />} /> 
       </Routes>
     </Router>
   );
