320e301f93b61649e1c9320a58471b44c58c335c
Dau Ba Anh Dung <anhdungkudo@gmail.com>
2025-06-26 22:43:22 +0700

    Create menu

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -2,12 +2,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
+import Menu from './pages/Menu';
 function App() {
   return (
     <Router>
       <Routes>
+        
         <Route path="/" element={<Home />} />
         <Route path="/booking" element={<Booking />} /> 
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </Router>
   );
