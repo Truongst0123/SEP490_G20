81ea39bcb05e775113c071109fb27f381148e5d7
Dau Ba Anh Dung <anhdungkudo@gmail.com>
2025-06-30 21:29:32 +0700

    Create Register page

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -4,8 +4,9 @@ import Booking from './pages/Booking';
 import Menu from './pages/Menu';  // Import trang Menu
 import Cart from './pages/Cart';  // Import trang Giỏ hàng
 import Payment from './pages/Payment'; // Import trang Thanh Toán
-import { CartProvider } from './contexts/CartContext';
 import Login from './pages/Login'; // Import trang Đăng Nhập
+import Register from './pages/Register'; // Import trang Đăng Ký
+import { CartProvider } from './contexts/CartContext';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />  {/* Route cho trang Giỏ hàng */}
           <Route path="/payment" element={<Payment />} /> {/* Route cho trang Thanh Toán */}
           <Route path="/login" element={<Login />} /> {/* Route cho trang Đăng Nhập */}
+          <Route path="/register" element={<Register />} /> {/* Route cho trang Đăng Ký */}
         </Routes>
       </Router>
     </CartProvider>
