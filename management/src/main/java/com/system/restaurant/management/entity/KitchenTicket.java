09aead9ab376444f33c4b3e55c7bcb028ce5f95f
Dung-L3 <donnylee2003@gmail.com>
2025-07-02 16:38:37 +0700

    Create KitchenTicket.java

---
+++ management/src/main/java/com/system/restaurant/management/entity/KitchenTicket.java
@@ -0,0 +1,29 @@
+package com.system.restaurant.management.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "KitchenTickets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KitchenTicket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TicketID")
+    private Integer ticketId;
+
+    @Column(name = "OrderID", nullable = false)
+    private Integer orderId;
+
+    @Column(name = "PrintedAt", nullable = false)
+    private LocalDateTime printedAt = LocalDateTime.now();
+
+    @Column(name = "PrintedBy")
+    private Integer printedBy;
+}
