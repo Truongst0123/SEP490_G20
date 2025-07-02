b9349bab130c542b14516de940e955869abe5046
Dung-L3 <donnylee2003@gmail.com>
2025-07-02 16:39:50 +0700

    Create UserRepository.java

---
+++ management/src/main/java/com/system/restaurant/management/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.system.restaurant.management.repository;
+
+import com.system.restaurant.management.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameAndStatus(String username, Boolean status);
+}
