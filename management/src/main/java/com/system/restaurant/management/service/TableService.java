d4a6dcc76b496da0bfcdebef76a70aad70aee394
Dung-L3 <donnylee2003@gmail.com>
2025-07-02 17:31:11 +0700

    update table service waiter

--- management/src/main/java/com/system/restaurant/management/service/TableService.java
+++ management/src/main/java/com/system/restaurant/management/service/TableService.java
@@ -1,6 +1,8 @@
 package com.system.restaurant.management.service;
 
 import com.system.restaurant.management.entity.RestaurantTable;
+import com.system.restaurant.management.entity.TableGroup;
+
 import java.util.List;
 
 public interface TableService {
@@ -9,5 +11,13 @@ public interface TableService {
     List<RestaurantTable> findAll();
     RestaurantTable update(Integer id, RestaurantTable table);
     void delete(Integer id);
+
+    //waiter
+    RestaurantTable updateTableStatus(Integer tableId, String status);
+    TableGroup splitTable(List<Integer> tableIds, Integer createdBy, String notes);
+    TableGroup mergeTable(List<Integer> tableIds, Integer createdBy, String notes);
+    List<RestaurantTable> getAvailableTables();
+    List<RestaurantTable> getTablesByStatus(String status);
+    List<RestaurantTable> getTablesByArea(Integer areaId);
 }
 
