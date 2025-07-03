7bc91b5adf955be297d2ad99c25c510e8fc4dbf8
Dung-L3 <donnylee2003@gmail.com>
2025-07-03 16:08:31 +0700

    Update ResourceNotFoundException.java

--- management/src/main/java/com/system/restaurant/management/exception/ResourceNotFoundException.java
+++ management/src/main/java/com/system/restaurant/management/exception/ResourceNotFoundException.java
@@ -10,4 +10,12 @@ public class ResourceNotFoundException extends RuntimeException {
     private final String resourceName;
     private final String fieldName;
     private final Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue));
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
