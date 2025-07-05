f1526541328e1d62338d823c77269bce1ecd8329
Dung-L3 <donnylee2003@gmail.com>
2025-07-05 14:49:59 +0700

    update table service

--- management/src/main/java/com/system/restaurant/management/repository/TableRepository.java
+++ management/src/main/java/com/system/restaurant/management/repository/TableRepository.java
@@ -4,6 +4,10 @@ import com.system.restaurant.management.entity.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TableRepository extends JpaRepository<RestaurantTable, Integer> {
+    List<RestaurantTable> findByStatus(String status);
+    List<RestaurantTable> findByAreaId(Integer areaId);
 }
